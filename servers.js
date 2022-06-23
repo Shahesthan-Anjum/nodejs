5ca425a56378d656ca0f976a523b17cc97a18224
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2022-06-23 17:55:29 +0530

    servers and read the data from readme.txt to browser program

--- servers.js
+++ servers.js
@@ -7,3 +7,17 @@ var server=http.createServer(function(req,res){
 
 server.listen(3000,'127.0.0.1');
 console.log("completed");
+
+
+var http=require('http');
+var fs = require('fs');
+
+var server=http.createServer(function(req,res){
+    console.log('request was made:' + req.url);
+    res.writeHead(200,{'Content-type':'text/plain'});
+    var myReadStream=fs.createReadStream(__dirname + '/readMe.txt','utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("completed from readMe.txt file to browser");
