4bbb0844f21f96f69dbd45a06558e60d1248a2a8
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2022-06-24 09:32:48 +0530

    3 programs in one file

--- servers.js
+++ servers.js
@@ -7,6 +7,7 @@ var server=http.createServer(function(req,res){
 
 server.listen(3000,'127.0.0.1');
 console.log("completed");
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 
 var http=require('http');
@@ -21,3 +22,23 @@ var server=http.createServer(function(req,res){
 
 server.listen(3000,'127.0.0.1');
 console.log("completed from readMe.txt file to browser");
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var http=require('http');
+var fs = require('fs');
+
+var server=http.createServer(function(req,res){
+    console.log('request was made:' + req.url);
+    res.writeHead(200,{'Content-type':'text/html'});
+    var myReadStream=fs.createReadStream(__dirname + '/index.html','utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000,'127.0.0.1');
+console.log("completed from readMe.txt file to browser");
+
+Output:
+
+
+
+
