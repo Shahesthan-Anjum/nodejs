435d6066a1196027df8b214e3f95017fb8282331
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2022-06-24 13:13:00 +0530

    updated

--- express.js
+++ express.js
@@ -4,8 +4,12 @@ app.get("/",(req, res) =>{
     res.send ("hello");
 });
 app.get('/contact',(req,res)=>{
-    res.send("This is homepage");
+    res.send("This is contactpage");
 });
+app.get('/profile/:id',(req,res)=>{
+    res.send("This is to request profile with id"+req.params.id);
+});
+
 app.listen("3000", ()=>{
     console.log("server started");
 });
