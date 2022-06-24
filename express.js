c72b6c0517912f04ab143f78d123dc597bf664dd
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2022-06-24 14:47:19 +0530

    updated

--- express.js
+++ express.js
@@ -13,3 +13,26 @@ app.get('/profile/:id',(req,res)=>{
 app.listen("3000", ()=>{
     console.log("server started");
 });
+Updated
+var express=require('express');
+var app=express();
+// app.get("/",(req, res) =>{
+//     res.send("hello");
+// });
+// app.get('/contact',(req,res)=>{
+//     res.send("This is contactpage");
+// });
+app.get("/",(req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+});
+app.get("/contact",(req, res) =>{
+    res.sendFile(__dirname+ '/contact.html');
+});
+app.get('/profile/:name',(req,res)=>{
+    res.send("This is to request profile with id and name " +req.params.name);
+});
+
+app.listen("3000", ()=>{
+    console.log("server started");
+});
+
