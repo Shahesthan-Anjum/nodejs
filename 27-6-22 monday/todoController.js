2f55f38c391789fbf59e132e9bd7f2dd7e199850
Shahesthan-Anjum <101544115+Shahesthan-Anjum@users.noreply.github.com>
2022-06-27 15:42:54 +0530

    uodated at 15:42(3:42)

--- 27-6-22 monday/todoController.js
+++ 27-6-22 monday/todoController.js
@@ -11,3 +11,22 @@ module.exports=function(app){
 
 
 };
+var data=[{item:'breakfast'},{item:'lunch'},{item:'dinner'}]
+var bodyParser=require('body-parser');
+var urlencodedParser=bodyParser.urlencoded({extended:false})
+module.exports=function(app){
+    app.get('/todo',function(req,res){
+        res.render('todo',{todos:data});
+    });
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data);
+    });
+    app.delete('/todo/:item',function(req,res){
+        data=data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item;
+        });
+    });
+
+
+};
